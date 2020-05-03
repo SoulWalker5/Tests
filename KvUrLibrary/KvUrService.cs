b8066b32a8328a4b5173b39205a389cf1dc70648
soulwalker5 <shrituon@gmail.com>
2020-05-03 21:41:11 +0300

    Added tests fot roots

--- KvUrLibrary/KvUrService.cs
+++ KvUrLibrary/KvUrService.cs
@@ -10,7 +10,7 @@ namespace KvUrLibrary
     {
         public double Discriminant(int a, int b, int c)
         {
-            if(a == int.MaxValue || b == int.MaxValue || c == int.MaxValue)
+            if (a == int.MaxValue || b == int.MaxValue || c == int.MaxValue)
             {
                 return 0;
             }
@@ -20,27 +20,48 @@ namespace KvUrLibrary
             return discriminant;
         }
 
-        public void Roots(int a, int b, double discriminant)
+        public double[] Roots(int a, int b, double discriminant)
         {
+            double[] result = new double[2];
+
+            if (a == int.MaxValue || b == int.MaxValue || discriminant == double.MaxValue)
+            {
+                return new double[] { 0d };
+            }
+
+            if (a == int.MinValue || b == int.MinValue || discriminant == double.MinValue)
+            {
+                return new double[] { 0d };
+            }
+
             if (discriminant < 0)
             {
                 Console.WriteLine("No roots");
+
+                return result;
             }
 
             else if (discriminant == 0)
             {
-                double x = (-b + Math.Sqrt(discriminant)) / 2 * a;
+                double x = (-b + Math.Sqrt(discriminant)) / (2 * a);
+                result[0] = x;
 
                 Console.WriteLine("Discriminant = {0}, than root = {1}", discriminant, x);
+
+                return result;
             }
 
-            else if (discriminant > 0)
+            else
             {
-                double x = (-b + Math.Sqrt(discriminant)) / 2 * a;
+                double x = (-b + Math.Sqrt(discriminant)) / (2 * a);
+                double x1 = (-b - Math.Sqrt(discriminant)) / (2 * a);
 
-                double x1 = (-b - Math.Sqrt(discriminant)) / 2 * a;
+                result[0] = x;
+                result[1] = x1;
 
                 Console.WriteLine("Discriminant = {0}, than roots = {1} and {2}", discriminant, x, x1);
+
+                return result;
             }
         }
     }
--- NUnitTest/Kv_Ur_UnitTest.cs
+++ NUnitTest/Kv_Ur_UnitTest.cs
@@ -21,6 +21,7 @@ namespace NUnitTest
         [TestCase(16, -8, 1, 0)]
         [TestCase(9, -6, 2, -36)]
         [TestCase(2, 3, 4, -23)]
+        [TestCase(9, 6, 3, -72)]
         [TestCase(int.MinValue, int.MinValue, int.MinValue, 0)]
         [TestCase(int.MaxValue, int.MaxValue, int.MaxValue, 0)]
         [TestCase(int.MinValue, int.MaxValue, 0, 0)]
@@ -30,6 +31,28 @@ namespace NUnitTest
 
             //Execute
             var result = _kvUrService.Discriminant(a, b, c);
+
+            //Assert
+
+            Assert.AreEqual(expected, result);
+        }
+
+        [Test]
+        [TestCase(9, 6, -72, new double[] { 0d, 0d })]
+        [TestCase(16, -8, 0, new double[] { 0.25d, 0d })]
+        [TestCase(2, 5, 81, new double[] { 1.0d, -3.5d })]
+        [TestCase(int.MinValue, 5, 81, new double[] { 0d })]
+        [TestCase(2, int.MinValue, 81, new double[] { 0d })]
+        [TestCase(2, int.MaxValue, 81, new double[] { 0d })]
+        [TestCase(int.MaxValue, 5, 81, new double[] { 0d })]
+        [TestCase(2, 5, double.MaxValue, new double[] { 0d })]
+        [TestCase(2, 5, double.MinValue, new double[] { 0d })]
+        public void Test2(int a, int b, double discriminant, double[] expected)
+        {
+            //Prepare
+
+            //Execute
+            var result = _kvUrService.Roots(a, b, discriminant);
 
             //Assert
 
